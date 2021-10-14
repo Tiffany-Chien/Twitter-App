e7a5799b08ce889f3bb52602af3162bb1aeafbdf
Tiffany <tiffanychien9@gmail.com>
2021-10-14 03:24:40 -0400

    add the favorite/unfavorite button

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -15,8 +15,41 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetsContent: UILabel!
     
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var favButton: UIButton!
     
     
+    var favorite: Bool = false
+    var tweetId: Int = -1
+    
+    func setFavorite(_ isFavorite:Bool) {
+        favorite = isFavorite;
+        if (favorite) {
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        } else { // if it's not favorited
+            // change back to gray
+            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        // already fav => fav
+        let toBeFavorited = !favorite
+        if (toBeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true) // change color
+            }, failure: { (error) in print("Favorite did not succeed: \(error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unFavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false) // change color
+            }, failure: { (error) in print("Unfavorite did not succeed \(error)")
+            })
+        }
+    }
+    
+    @IBAction func retweet(_ sender: Any) {
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
